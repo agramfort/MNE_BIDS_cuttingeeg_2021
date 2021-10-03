68ea71f34ccc3323523481e051f181805ae05e7a
Alexandre Gramfort <alexandre.gramfort@m4x.org>
2021-10-03 21:18:37 +0200

    update config

--- config_ds000248.py
+++ config_ds000248.py
@@ -1,22 +1,28 @@
 study_name = 'ds000248'
 bids_root = './ds000248'
-deriv_root = f'{bids_root}/derivatives/mne-bids-pipeline/'
+deriv_root = f'{bids_root}/../derivatives/mne-bids-pipeline/'
 subjects_dir = f'{bids_root}/derivatives/freesurfer/subjects'
 
 subjects = ['01']
-rename_events = {'Smiley': 'Emoji',
-                 'Button': 'Switch'}
 conditions = ['Auditory', 'Visual', 'Auditory/Left', 'Auditory/Right']
 contrasts = [('Visual', 'Auditory'),
              ('Auditory/Right', 'Auditory/Left')]
 
-time_frequency_conditions = ['Auditory', 'Visual']
+decode = False  # True if you want to do time by time decoding
+
+# Uncomment next for time-frquency
+# time_frequency_conditions = ['Auditory', 'Visual']
 
 ch_types = ['meg']
-mf_reference_run = '01'
-find_flat_channels_meg = True
-find_noisy_channels_meg = True
-use_maxwell_filter = True
+reject = dict(grad=4000e-13, mag=4e-12, eog=150e-6)  # this can be highly data dependent
+# reject = "autoreject_global"
+
+# Maxwell filter (uncomment if needed)
+# mf_reference_run = '01'
+# find_flat_channels_meg = True
+# find_noisy_channels_meg = True
+# use_maxwell_filter = True
+
 process_er = True
 noise_cov = 'emptyroom'
 
@@ -29,3 +35,5 @@ eog_proj_from_average = False
 bem_mri_images = 'FLASH'
 recreate_bem = False  # set to True if you don't have any
 recreate_scalp_surface = False  # set to True if you don't have any
+
+on_error = 'debug'
